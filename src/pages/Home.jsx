11282441e196cf2a854d63fc72b0004311ba9d10
Abiodun <emmi34.et@gmail.com>
2023-03-03 22:00:36 +0100

    updated van page and added miragejs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -14,10 +15,12 @@ const Home = () => {
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <button
-          className="bg-[#FF8C38] h-[50px] w-[70%] lg:w-1/4 rounded-md font-inter font-bold text-white">
-          Find your van
-        </button>
+        <Link
+          to="/vans"
+          className="bg-[#FF8C38] h-[50px] w-[70%] lg:w-1/4 rounded-md font-inter font-bold text-white flex justify-center items-center"
+        >
+          <button>Find your van</button>
+        </Link>
       </section>
       <Footer />
     </>
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer';
 
 const Vans = () => {
+
+  const [vans, setVans] = useState([])
+
+  useEffect(() => {
+    fetch("/api/vans")
+    .then((res) => res.json())
+    .then((json) => {
+        setVans(json.vans)
+    })
+  },[])
+
+
   return (
     <div>
       <Navbar />
-      <section className="flex justify-center items-center h-[400px]">
-        <h1 className="text-xl">
-          This page is not yet available😥, You can explore the other page😁
-        </h1>
+      <h1 className='font-inter font-bold text-xl p-4'>Explore our van options</h1>
+      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center items-center p-4 font-inter">
+        {vans.map((van) => (
+          <div key={van.id}>
+            <img src={van.imageUrl} alt="" className="rounded" />
+            <div className="flex justify-between">
+              <p className="text-[12px] font-semibold">{van.name}</p>
+              <p className="text-[12px] font-semibold">
+                ${van.price} <br />{" "}
+                <span className="text-[10px] font-light">/day</span>
+              </p>
+            </div>
+            <button className={`van-type ${van.type} selected`}>
+              {van.type}
+            </button>
+          </div>
+        ))}
       </section>
       <Footer />
     </div>
